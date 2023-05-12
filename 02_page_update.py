ddc1cb17b4b30418ed3efd1d71e56b08dc330ed3
Yeltsin <ychuquilinc@unc.edu.pe>
2023-05-12 13:14:59 -0500

    12/05/2023

---
+++ 02_page_update.py
@@ -0,0 +1,16 @@
+import time
+
+import flet as ft
+
+
+def main(page: ft.Page):
+    t = ft.Text()
+    page.add(t)  # it's a shortcut for page.controls.append(t) and then page.update()
+
+    for i in range(10):
+        t.value = f"Step {i}"
+        page.update()
+        time.sleep(1)
+
+ft.app(target=main)
+
