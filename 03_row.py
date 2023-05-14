e89d6e34a468c544b8b69b3184fac6a34ce1d82f
Yeltsin <ychuquilinc@unc.edu.pe>
2023-05-13 23:38:38 -0500

    13/05/2023

---
+++ 03_row.py
@@ -0,0 +1,11 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(
+        ft.Row(controls=[
+            ft.Text("A"),
+            ft.Text("B"),
+            ft.Text("C")
+        ])
+    )
+ft.app(target=main)
---
+++ 04_TextField_ElevatedButton.py
@@ -0,0 +1,11 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(
+        ft.Row(controls=[
+            ft.TextField(label="Your name"),
+            ft.ElevatedButton(text="Say my name!")
+        ])
+    )
+
+ft.app(target=main)
